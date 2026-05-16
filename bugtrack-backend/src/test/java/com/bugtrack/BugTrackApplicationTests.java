1e4815f67f3f54145c443f658859bae9e79fce1d
Anshuman Giri <anshumangiriofficial@gmail.com>
2026-05-16 21:57:31 +0530

    chore: fix test class, change port to 8081, verify Spring Boot run

---
+++ bugtrack-backend/src/test/java/com/bugtrack/BugTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
